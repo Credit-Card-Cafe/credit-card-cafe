0ac8e228a07e4192bd077798f12dac6459138334
Ben Keil <keilben8@gmail.com>
2023-10-16 11:51:12 -0700

    First commit from new device

--- src/routes/contribute/update/[slug]/+page.svelte
+++ src/routes/contribute/update/[slug]/+page.svelte
@@ -2,11 +2,15 @@
 <script lang="js">
     import UpdateCard from './UpdateCard.svelte';
     export let data;
+    import { user } from '../../../../lib/stores';
   </script>
   
-  
-  {#if data.card}
-    <UpdateCard card={data.card}></UpdateCard>
+  {#if $user}
+    {#if data.card}
+      <UpdateCard card={data.card}></UpdateCard>
+    {:else}
+      <div>Credit Card not found. Would you like to create one?</div>
+    {/if}
   {:else}
-    <div>Credit Card not found. Would you like to create one?</div>
+    <div>Please log in to update a card</div>
   {/if}
