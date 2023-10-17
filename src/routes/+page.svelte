5d3ce9f41dc598088303bff7ff138f7a4718dc0c
Ben Keil <keilben8@gmail.com>
2023-10-17 13:40:42 -0400

    Search Page includes table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,11 @@
 </svelte:head>
 
 <script lang="js">
-  import { cardList } from '../lib/stores';
   import "./global.css";
 </script>
 
 <div>Welcome to Credit Card DB</div>
 <div>Here you can find advanced details about credit cards</div>
-
-{#each $cardList as card}
-  <a href="/card/{card.url}">
-    <span>{card.bank}</span> -
-    <span>{card.name}</span>
-  </a><br>
-{/each}
 
 
 <style>
---
+++ src/routes/search/components/SearchTable.svelte
@@ -0,0 +1,43 @@
+<script>
+    import { dataField } from "../../../lib/fields";
+    import { cardList } from "../../../lib/stores";
+    export let queries;
+
+    var searchResults = $cardList;
+
+    function sortCards() {
+        searchResults.sort();
+    }
+</script>
+
+<table>
+    <tr>
+        <th on:click={sortCards()}>Card &darr;</th>
+        <th>Bank</th>
+        {#each queries as query}
+            <th>{dataField[query].name}</th>
+        {/each}
+    </tr>
+    {#each searchResults as card}
+    
+    <tr>
+        <td><a href="/card/{card.url}">{card.name} </a></td>
+        <td>{card.bank}</td>
+        {#each queries as query}
+        <td>{card[query]}</td>
+        {/each}
+    </tr>
+       
+    
+    {/each}
+</table>
+
+<style>
+    table > tr:nth-child(even) {
+  background-color: rgb(240, 251, 255);
+}
+th,td {
+    padding: 5px;
+    cursor: pointer;
+}
+</style>
