a9d98cf4fb5e709e3443822071b1860d77de17c7
Ben Keil <keilben8@gmail.com>
2023-10-20 16:46:39 -0400

    Added NumberInput

---
+++ src/routes/contribute/update/[slug]/components/NumberInput.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let field;
+    export let value = undefined;
+    export let object = false;
+    import { dataField } from "../../../../../lib/fields";
+    import { newCard } from "../../../../../lib/stores";
+
+    const defaultValue = value;
+
+    function setUpdate() {
+        if (object) {
+            if (!Object.hasOwn($newCard, object)) {
+                $newCard[object] = {};
+            }
+            $newCard[object][field] = value;
+            if (value == defaultValue || (!defaultValue && value == "")) {
+                delete $newCard[object][field];
+                if (Object.keys($newCard[object]).length == 0)
+                    delete $newCard[object];
+            }
+        } else {
+            $newCard[field] = value;
+            if (value == defaultValue || (!defaultValue && value == "")) {
+                delete $newCard[field];
+            }
+        }
+        
+    }
+</script>
+
+
+<div>
+    {#if object} {dataField[object]["data"][field].name} {:else} {dataField[field].name} {/if}
+    <input type="number" bind:value on:input={() => setUpdate()}>
+</div>
