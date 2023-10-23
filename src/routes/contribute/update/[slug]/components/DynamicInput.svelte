fed9ec4dbe0aeaa26e1b76266819568d4a9cb370
bkeil <37550434+nuetu@users.noreply.github.com>
2023-10-23 12:40:58 -0400

    Dynamic Input push, temporary

---
+++ src/routes/contribute/update/[slug]/components/DynamicInput.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let field;
+    export let object;
+    import { dataField } from "../../../../../lib/fields";
+    import { newCard } from "../../../../../lib/stores";
+
+    if (object == undefined) {
+        object = {};
+        for(let prop of Object.keys(dataField[field]["data"])) {
+            object[prop] = "";
+        }
+    }
+
+</script>
+
+<div>
+    {#each Object.keys(object) as prop}
+        
+    {/each}
+</div>
+
+<style>
+
+</style>
