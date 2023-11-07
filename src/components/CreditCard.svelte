5d07f0df05c9c211b0ef3c70a22967aec8171c27
Ben Keil <keilben8@gmail.com>
2023-11-06 19:48:54 -0500

    Card background is off-white, cards with no pyhsical image will not display transparent when stacked. Fixed issue #64

--- src/components/CreditCard.svelte
+++ src/components/CreditCard.svelte
@@ -68,8 +68,8 @@
     }
     #creditCard {
         position: relative;
-        background-color: var(--color, rgb(253,248,244));
-        background: linear-gradient(146deg, rgb(var(--color)), rgba(var(--color), 0.6), rgb(var(--color)));
+        background: rgb(253,248,244);
+        background-image: linear-gradient(146deg, rgb(var(--color)), rgba(var(--color), 0.6), rgb(var(--color)));
         width: 323px;
         height: 186px;
         padding: 25px;
