c026bd5f3627b8cca550e81bafaeaa73a6838705
Ben Keil <keilben8@gmail.com>
2023-11-09 20:53:47 -0500

    Whatever, heres firebase

---
+++ firebaseconfig.js
@@ -0,0 +1,9 @@
+//Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyAHdRUzJTM-_3JQr4LW9fYS3935w28FHKw",
+    authDomain: "creditcarddb-3fafd.firebaseapp.com",
+    projectId: "creditcarddb-3fafd",
+    storageBucket: "creditcarddb-3fafd.appspot.com",
+    messagingSenderId: "992368058830",
+    appId: "1:992368058830:web:393c3331148baba69a8b9f",
+  };
