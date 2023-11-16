4eca6ce256790d50508acc1342dd256e5961617d
Ben Keil <keilben8@gmail.com>
2023-11-16 12:08:50 -0500

    Added info card for card hovers in card stack

---
+++ src/components/CardInfoShort.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let card;
+    export let left = false;
+</script>
+
+<div id="cis" class={left?"left":"right"}> 
+    <b>{card.name}</b>
+    <b>{card.bank}</b>
+</div>
+
+<style>
+    #cis {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        -webkit-transform:scale(0.9);
+        -moz-transform:scale(0.9);
+        -ms-transform:scale(0.9);
+        transform:scale(0.9);
+        background: rgb(253,248,244);
+        border:1px solid black;
+        width: 323px;
+        height: 185px;
+        padding: 25px;
+    }
+    #cis.right {
+        border-radius: 0 18px 18px 0;
+        padding-left: 40px;
+    }
+    #cis.left {
+        border-radius: 18px 0 0 18px;
+        padding-right: 40px
+    }
+</style>
