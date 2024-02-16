68cf054900fa697cf7697140ed4cf2723e64eda3
Ben Keil <keilben8@gmail.com>
2024-02-16 15:35:39 -0500

    Obvious Objects :)

--- src/routes/search/components/TableQuery.svelte
+++ src/routes/search/components/TableQuery.svelte
@@ -21,6 +21,7 @@
     <input id={`query${object + field}`} class="hidden" type="checkbox" value={dataField[object]["data"][field].name} on:change={() => addQuery()}>
     <label for={`query${object + field}`} class={`label ${isChecked ? "grn dark:text-gray-900 text-alt" : "bg-gray-200 hover:bg-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700"}`}>{dataField[object]["data"][field].name}</label> 
 {:else if isObject}
+    <div class={`flex flex-row flex-wrap justify-center ${isChecked ? "bg-black/[0.1] dark:bg-black/[0.3] mx-1 rounded-xl lg:rounded-full py-1 lg:py-0" : ""}`}>
     <input id={`query${field}`} class="hidden" type="checkbox" value={field} on:change={() => addQuery()}>
     <label for={`query${field}`} class={`label ${isChecked ? "blu dark:text-gray-900 text-alt" : "bg-gray-200 hover:bg-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700"}`}>{dataField[field].name}</label>
     {#if isChecked} 
@@ -28,6 +29,7 @@
             <svelte:self field={f} object={field} bind:queries={queries}/>
         {/each}
     {/if}
+    </div>
 {:else}
     <input id={`query${field}`} class="hidden" type="checkbox" value={field} on:change={() => addQuery()}>
     <label for={`query${field}`} class={`label ${isChecked ? "grn dark:text-gray-900 text-alt" : "bg-gray-200 hover:bg-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700"}`}>{dataField[field].name}</label>
@@ -36,6 +38,9 @@
 <style>
 .label {
     @apply text-center py-1 px-4 rounded-full inline-block mx-1 my-1 transition-all cursor-pointer;
+}
+.objlabel {
+    @apply text-center py-0.5 px-3 rounded-full inline-block mx-1 my-1 transition-all cursor-pointer;
 }
 .grn {
     @apply bg-green-500 ;
